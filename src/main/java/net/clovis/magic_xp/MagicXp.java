ab17b2bea02ab4363727c1752943c90559a5a7b5
ClovisLd <clovis.leden@gmail.com>
2022-12-03 22:52:21 +0100

    3/12

--- src/main/java/net/clovis/magic_xp/MagicXp.java
+++ src/main/java/net/clovis/magic_xp/MagicXp.java
@@ -10,13 +10,13 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(magic_xp.MOD_ID)
-class magic_xp
+@Mod(MagicXp.MOD_ID)
+public class MagicXp
 {
     public static final String MOD_ID = "magic_xp";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public magic_xp()
+    public MagicXp()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
---
+++ src/main/java/net/clovis/magic_xp/item/Moditem.java
@@ -0,0 +1,9 @@
+package net.clovis.magic_xp;
+
+import net.minecraft
+
+public class Moditem
+{
+    public static final DeferredRegister<Item> ITEM =
+            DeferredRegister
+}
